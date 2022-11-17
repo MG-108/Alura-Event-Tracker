bb334bdfdecd390031312c43647ca5f8b1bc392b
Matheus Ghignatti <matheusghignatti@gmail.com>
2022-11-17 16:37:04 -0300

    3

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
-import { listaDeEventosState } from "../../state/atom";
+import useListaDeEventos from "../../state/hooks/useListaDeEventos";
 
 import Evento from "../Evento";
 import Filtro from "../Filtro";
@@ -9,7 +8,7 @@ import style from "./ListaDeEventos.module.scss";
 const ListaDeEventos: React.FC<{
   aoFiltroAplicado: (data: Date | null) => void;
 }> = ({ aoFiltroAplicado }) => {
-  const eventos = useRecoilValue(listaDeEventosState);
+  const eventos = useListaDeEventos();
 
   return (
     <section>
